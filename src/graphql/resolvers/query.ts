3e6dcbca3fca798c9dc43c4101eea603447078dc
@Wil <emma.wilinston@gmail.com>
2021-11-08 10:09:06 -0500

    query allMenu listo

--- src/graphql/resolvers/query.ts
+++ src/graphql/resolvers/query.ts
@@ -1,3 +1,15 @@
+import { ContextType } from '../../interface/common'
+
 export const Query = {
-  hello: () => 'holamundo',
+  allMenu: async (parent: void, args: void, context: ContextType) => {
+    const { Models } = context
+    const { Menu } = Models
+
+    try {
+      return await Menu.find({})
+    } catch (err) {
+      console.log(err)
+      return []
+    }
+  },
 }
---
+++ src/interface/common.ts
@@ -0,0 +1,7 @@
+export interface ContextType {
+  Models: ModelsType
+}
+
+interface ModelsType {
+  Menu: any
+}
